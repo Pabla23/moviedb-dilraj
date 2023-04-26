6c0162427c72b7edebb385e547f84c130307e1f6
Pabla23 <dilraj_pabla@hotmail.com>
2023-04-25 19:24:09 -0700

    added mobile & desktop nav menu

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,32 @@
 // Nav
 import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
 
-const Nav = () => {
+function Nav() {
+    const [isOpen, setIsOpen] = useState(false);
+  
+    const toggleMenu = () => {
+      setIsOpen(!isOpen);
+    }
+
+    const closeMenu = () => {
+        setIsOpen(false);
+    }
+    
     return (
-        <nav className="main-nav">
-            <ul>
-                <li><NavLink to="/">Home</NavLink></li>
-                <li><NavLink to="/about">About</NavLink></li>
-                <li><NavLink to="/favourites">Favourites</NavLink></li>
-            </ul>
-        </nav>
+      <nav className={`main-nav ${isOpen ? 'open' : ''}`}>
+          <button className={`nav-toggle ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
+              <span></span>
+              <span></span>
+              <span></span>
+          </button>
+          <ul className={`nav-menu ${isOpen ? 'open' : ''}`}>
+              <li><NavLink to="/" activeClassName="active" onClick={closeMenu}>Home</NavLink></li>
+              <li><NavLink to="/about" activeClassName="active" onClick={closeMenu}>About</NavLink></li>
+              <li><NavLink to="/favourites" activeClassName="active" onClick={closeMenu}>Favourites</NavLink></li>
+          </ul>
+      </nav>
     );
-};
+  }
 
 export default Nav;
